fbb2a885d8f1753b91481c35003c96d385e7e16c
Alec Jensen <59067840+alec-jensen@users.noreply.github.com>
2023-03-23 20:15:43 -0500

    efefa

--- setup.py
+++ setup.py
@@ -12,43 +12,6 @@ python setup.py sdist bdist_wheel
 from setuptools import setup
 from pafy import __version__
 
-
-def install_deps():
-    """
-    FROM hnykda ON GITHUB: https://github.com/pypa/pip/issues/3610#issuecomment-356687173
-    Reads requirements.txt and preprocess it
-    to be feed into setuptools.
-
-    This is the only possible way (we found)
-    how requirements.txt can be reused in setup.py
-    using dependencies from private github repositories.
-
-    Links must be appendend by `-{StringWithAtLeastOneNumber}`
-    or something like that, so e.g. `-9231` works as well as
-    `1.1.0`. This is ignored by the setuptools, but has to be there.
-
-    Warnings:
-        to make pip respect the links, you have to use
-        `--process-dependency-links` switch. So e.g.:
-        `pip install --process-dependency-links {git-url}`
-
-    Returns:
-         list of packages and dependency links.
-    """
-    default = open('requirements.txt', 'r').readlines()
-    new_pkgs = []
-    links = []
-    for resource in default:
-        if 'git+ssh' in resource:
-            pkg = resource.split('#')[-1]
-            links.append(resource.strip() + '-9876543210')
-            new_pkgs.append(pkg.replace('egg=', '').rstrip())
-        else:
-            new_pkgs.append(resource.strip())
-    return new_pkgs, links
-
-pkgs, new_links = install_deps()
-
 setup(
     name='pafy',
     packages=['pafy'],
@@ -60,11 +23,12 @@ setup(
     author_email="np1nagev@gmail.com",
     url="https://github.com/mps-youtube/pafy/",
     download_url="https://github.com/mps-youtube/pafy/tags",
+    install_requires=[
+        "git+https://github.com/ytdl-org/youtube-dl.git@master#egg=youtube_dl"
+    ],
     extras_require={
         'youtube-dl-backend': ["youtube-dl"],
     },
-    install_requires=pkgs,
-    dependency_links=new_links,
     package_data={"": ["LICENSE", "README.rst", "CHANGELOG", "AUTHORS"]},
     include_package_data=True,
     license='LGPLv3',
